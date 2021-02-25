9ddc9eaf061d477fa9a891d6907b9309d0f6b4df
이현주 <hyunmail521@gmail.com>
2021-02-25 22:56:44 +0900

    타입스크립트의 첫번째 좋은점과 jsDoc의 표준문법 알아보기

--- 01/app.js
+++ 01/app.js
@@ -9,9 +9,39 @@ var address = document.querySelector('#address');
 // user data
 var user = {};
 
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {string} address
+ */
+
+ //타입 객체에 대한 property를 정의할 수 있다.
+
+/**
+ * @returns {Promise<User>}
+ */ 
+
+// 이게 jsDoc에 표준 문법이다.
+
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.address.
+})
+
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser()
     .then(function (response) {
       // console.log(response);
       user = response.data;
