a1c003de95685aff6bc0e2feb166a6da053fcb59
leemember <hyunmail521@gmail.com>
2021-03-10 16:56:02 +0900

    인터페이스 제너릭을 이용해 인터페이스 코드들을 최적화

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,15 +1,21 @@
-//이전에 긴 코드의 타입선언들을 인터페이스에 담아놓기
-interface Email {
-  value: string;
+interface DropdownItem<T> {
+  value: T;
   selected: boolean;
-}
+} // 👩🏻‍🦳 =>인터페이스의 제너릭을 이용해서 두 가지 타입을 커버할 수 있게 되었다. (최장점)
+
+//이전에 긴 코드의 타입선언들을 인터페이스에 담아놓기
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
 
 
 //이 처럼 함수를 선언해줄때 타입을 정확하게 구체적으로 입력해주었다.
 // value = 문자열 , selected = 논리형태
 //그리고 배열 함수니까
 //const emails: {value: string; selected: boolean}[] 이렇게 [] 배열 표시도 빠트리면 안된다.
-const emails: {value: string; selected: boolean}[] = [
+//👩🏻 DropdownItem => 문자열 타입
+const emails: DropdownItem<string>[] = [
   //value와 텍스트가 같은 값을 쓰인다.
   //selected의 옵션값도 true/false로 구분한다.
   {value: 'naver.com', selected: true},
@@ -17,21 +23,22 @@ const emails: {value: string; selected: boolean}[] = [
   {value: 'hanmail.net', selected: false},
 ];
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
 
 //향후에 어떤 타입이 오던간에 다 수용할 수 있도록
 //매번 타입에 대한 타입정의를 인터페이스를 통해서 하는 것이 아니라
 //타입이 어떤게 오던간에 그때그때마다 유연하게 갖다쓰겠다고 하는 것을
 // 🧔🏻 제네릭을 변환해서 사용할 수 있게 된다 !!!!!!!!
-interface TrueFalse {
-  value: boolean;
-  selected: boolean;
-}
+// interface TrueFalse {
+//   value: boolean;
+//   selected: boolean;
+// }
 
-const numberOfProucts:ProductNumber[] = [
+//👩🏻 DropdownItem => 넘버 타입
+const numberOfProucts:DropdownItem<number>[] = [
   {value: 1, selected: true},
   {value: 2, selected: false},
   {value: 3, selected: false},
@@ -39,8 +46,8 @@ const numberOfProucts:ProductNumber[] = [
 
 //이 함수에 배열의 값들을 넘겨받는다.
 //이 함수 괄호에 있는 item은 옵션을 뜻한다.
-function createDropdownItem(
-  item: Email | ProductNumber
+function createDropdownItem<T>(
+  item: DropdownItem<T> | DropdownItem<number>
   // | {value: number; selected: boolean} // item에는 얘도 받을 수 있고
   // | {value: string; selected: boolean} // 얘도 받을 수 있다.
   // 이 2개 다 수용할 수 있다. 이것을 유니온타입이라고 한다.
@@ -54,7 +61,7 @@ function createDropdownItem(
 // NOTE: 이메일 드롭 다운 아이템 추가
 // forEach : 반복문을 돌려준다.
 emails.forEach(function(email) {
-  const item = createDropdownItem(email); 
+  const item = createDropdownItem<string>(email); 
   // createDropdownItem는 value : number
   // createDropdownItem 함수에 item에 타입을 넣으니 에러가 안생긴다
   // 그냥 타입은 코드수가 길어서 인터페이스로 만들어 넣었다.
